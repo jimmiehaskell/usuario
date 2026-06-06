1bad5fe0b487d4e51801f09b28884f463f147a00
Jimmie Haskell <haskell4228@gmail.com>
2026-06-06 20:35:56 -0300

    Adiciona método que salva dados de usuário

---
+++ src/main/java/dev/jimmiehaskell/usuario/business/dto/TelefoneDTO.java
@@ -0,0 +1,14 @@
+package dev.jimmiehaskell.usuario.business.dto;
+
+import jakarta.persistence.Column;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TelefoneDTO {
+    private String numero;
+    private String ddd;
+}
