912dbadd8cb39fbbadb7da0c3efe67873c7776e4
Jimmie Haskell <haskell4228@gmail.com>
2026-06-06 19:50:25 -0300

    Adicionado classes de security

---
+++ src/main/java/dev/jimmiehaskell/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package dev.jimmiehaskell.usuario.infrastructure.repository;
+
+import dev.jimmiehaskell.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
---
+++ src/main/java/dev/jimmiehaskell/usuario/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package dev.jimmiehaskell.usuario.infrastructure.security;
+
+import dev.jimmiehaskell.usuario.infrastructure.entity.Usuario;
+import dev.jimmiehaskell.usuario.infrastructure.repository.UsuarioRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    // Repositório para acessar dados de usuário no banco de dados
+    private final UsuarioRepository usuarioRepository;
+
+    // Implementação do método para carregar detalhes do usuário pelo e-mail
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
+        // Busca o usuário no banco de dados pelo e-mail
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
+
+        // Cria e retorna um objeto UserDetails com base no usuário encontrado
+        return org.springframework.security.core.userdetails.User
+                .withUsername(usuario.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuario.getSenha()) // Define a senha do usuário
+                .build(); // Constrói o objeto UserDetails
+    }
+}
